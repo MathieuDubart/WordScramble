dbb097fe0275ca2b4a02c2d63eef64edebfb9b79
Mathieu Dubart <mathieudubart@mini-de-mathieu.home>
2023-08-06 15:58:53 +0200

    :sparkles: initial commit

---
+++ WordScramble/ContentView.swift
@@ -0,0 +1,119 @@
+//
+//  ContentView.swift
+//  WordScramble
+//
+//  Created by Mathieu Dubart on 06/08/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var usedWords = [String]()
+    @State private var rootWord = "" //the one they're spelling from eight letter word
+    @State private var newWord = "" //bind to text field to store word while typing
+    
+    @State private var errorTitle = ""
+    @State private var errorMessage = ""
+    @State private var showingError = false
+    
+    var body: some View {
+        NavigationView {
+            List {
+                Section {
+                    TextField("Enter your word", text: $newWord)
+                        .autocapitalization(.none)
+                }
+                
+                Section {
+                    ForEach(usedWords, id: \.self) { word in
+                        HStack {
+                            Image(systemName: "\(word.count).circle")
+                            Text(word)
+                        }
+                    }
+                }
+            }
+            .navigationTitle(rootWord)
+            .navigationBarTitleDisplayMode(.large)
+            .onSubmit(addNewWord)
+            .onAppear(perform: startGame)
+        }
+        .alert(errorTitle, isPresented: $showingError) {
+            Button("OK", role:.cancel){}
+        } message: {
+            Text(errorMessage)
+        }
+    }
+    
+    func addNewWord() {
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+        guard answer.count > 0 else { return }
+        
+        guard !isAlreadyUsed(word: answer) else {
+            wordError(title: "World already used", message: "Try an other word")
+            return
+        }
+        
+        guard isPossible(word: answer) else {
+            wordError(title: "Word isn't possible", message: "Can't spell that word from '\(rootWord)'")
+            return
+        }
+        
+        guard isExisting(word: answer) else {
+            wordError(title: "Word doesn't exist", message: "Try an existing one")
+            return
+        }
+        
+        newWord = ""
+    }
+    
+    func startGame() {
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            if let startWords = try? String(contentsOf: startWordsURL) {
+                let allWords = startWords.components(separatedBy: "\n")
+                rootWord = allWords.randomElement() ?? "silkworm"
+                return
+            }
+        }
+
+        fatalError("Could not load start.txt from bundle.")
+    }
+    
+    func isAlreadyUsed(word: String) -> Bool {
+        usedWords.contains(word) ? true : false
+    }
+    
+    func isPossible(word: String) -> Bool {
+        var tempWord = rootWord
+        
+        for letter in word {
+            if let position = tempWord.firstIndex(of: letter) {
+                tempWord.remove(at: position)
+            } else {
+                return false
+            }
+        }
+        
+        return true
+    }
+    
+    func isExisting(word: String) -> Bool {
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        
+        return misspelledRange.location == NSNotFound
+    }
+    
+    func wordError(title: String, message: String) {
+        errorTitle = title
+        errorMessage = message
+        showingError = true
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
